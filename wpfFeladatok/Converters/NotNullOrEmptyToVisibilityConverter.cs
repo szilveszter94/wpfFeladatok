fcac538c56d1ca48a9bee643b3c01f68d55488be
Szilveszter <s.szilveszter1994@gmail.com>
2024-05-02 12:52:36 +0200

    fix the converter bug

---
+++ wpfFeladatok/Converters/NotNullOrEmptyToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace wpfFeladatok.Converters
+{
+    public class NotNullOrEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return string.IsNullOrEmpty(str) ? Visibility.Hidden : Visibility.Visible;
+            }
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- wpfFeladatok/MainWindow.xaml.cs
+++ wpfFeladatok/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Data;
-using wpfFeladatok.ViewModels;
 
 namespace wpfFeladatok
 {
