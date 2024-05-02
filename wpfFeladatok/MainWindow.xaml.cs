8707db8d9afec9d55d48f7445cf405a994541622
Szilveszter <s.szilveszter1994@gmail.com>
2024-05-02 10:52:26 +0200

    Finish the first paper

---
+++ wpfFeladatok/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+using wpfFeladatok.ViewModels;
+
+namespace wpfFeladatok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _textContent = "";
+
+        public string TextContent
+        {
+            get { return _textContent; }
+            set
+            {
+                if (_textContent != value)
+                {
+                    _textContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
