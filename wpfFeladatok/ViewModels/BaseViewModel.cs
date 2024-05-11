704ab0eb1c453ee4d2c6ae06a037743832433584
Szilveszter <s.szilveszter1994@gmail.com>
2024-05-11 11:27:00 +0200

    remove virtual from baseview

--- wpfFeladatok/ViewModels/BaseViewModel.cs
+++ wpfFeladatok/ViewModels/BaseViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows;
 
 namespace wpfFeladatok.ViewModels
 {
@@ -8,7 +7,7 @@ namespace wpfFeladatok.ViewModels
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
